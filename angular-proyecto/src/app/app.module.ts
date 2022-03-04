6c4455ec48b7923628671107db25f68e3c5b037c
JuanS1471 <86208230+JuanS1471@users.noreply.github.com>
2022-03-03 19:40:32 -0500

    Se implementan componentes y carpetas con modulos

--- angular-proyecto/src/app/app.module.ts
+++ angular-proyecto/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { ExampleComponent } from './example/example.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    CoreModule,
+    SharedModule,
+    AppComponent,
+    ExampleComponent
   ],
   imports: [
     BrowserModule
