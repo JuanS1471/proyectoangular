9ad217997b30d2d6b5cfbddcbd77f5a04bbdd04b
JuanS1471 <86208230+JuanS1471@users.noreply.github.com>
2022-03-09 08:46:34 -0500

    Se crea componente de titulo

---
+++ angular-proyecto/src/app/shared/components/index.ts
@@ -0,0 +1,10 @@
+//  Import components
+import { TitleH1Component } from '../../../../../../../Guia/src/app/shared/components/title-h1/title-h1.component';
+
+
+export const components: any[] = [
+    TitleH1Component
+];
+
+// export all components
+export * from '../../../../../../../Guia/src/app/shared/components/title-h1/title-h1.component';
