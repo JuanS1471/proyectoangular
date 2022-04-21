2e26c859ade909ea95c53ddb33da758b4f78b6ef
JuanS1471 <86208230+JuanS1471@users.noreply.github.com>
2022-04-21 08:42:03 -0500

    Formularios con ngModel

--- angular-proyecto/src/app/modules/auth/login/login.component.ts
+++ angular-proyecto/src/app/modules/auth/login/login.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  public loginForm: { 
+    email: {
+      val: string;
+      error: string;
+      isValid: () => boolean
+    },
+    password: {
+      val: string;
+      error: string;
+      isValid: () => boolean
+    }
+  };
+  constructor() {
+    this.loginForm = {
+      email: {
+        val: '',
+        error: '*el email es necesario',
+        isValid(){
+          const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\-[^<>()\[\]\\.,;:\s@"]+)*)](".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\. [0-9]{1,3}\. [0-9]$/
+        
+          if(this.val === ''){
+            this.error = '*El email es requerido'
+          }else {
+            this.error = '*El Email no es valido'
+          }
+          return pattern.test(this.val);
+        }
+      },
+      password: {
+        val: '',
+        error: '*La contraseña es requerida',
+        isValid(){
+          return(this.val.length > 0);
+        }
+      }
+    }
+  }
 
-  ngOnInit(): void {
+  get isValidForm(){
+    return(this.loginForm.email.isValid() && this.loginForm.password.isValid());
   }
 
 }
