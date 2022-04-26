080f04ff481b6f984f18fbed80b80a7c823d2019
JuanS1471 <86208230+JuanS1471@users.noreply.github.com>
2022-04-26 08:38:20 -0500

    SubPropiedades formcontrol

--- angular-proyecto/src/app/modules/auth/login/login.component.ts
+++ angular-proyecto/src/app/modules/auth/login/login.component.ts
@@ -8,16 +8,23 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class LoginComponent {
 
-  public loginForm: FormGroup;
+  public loginForm;
     constructor( private FormBuilder: FormBuilder){
      
       this.loginForm = this.FormBuilder.group({
         email: ['', [Validators.required, Validators.pattern('^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')]],
-        password: ['', [Validators.required]]
+        password: ['', [Validators.required]],
+        person: this.FormBuilder.group({
+          name: ['',Validators.required, Validators.maxLength(35)],
+          lastname: ['',Validators.required, Validators.maxLength(35)]
+        })
       })
     }
+    get fp(){
+      return this.loginForm.controls.person.controls;
+    }
   autenticate(){
-    console.log('autenticate')
+    console.log('autenticated', this.loginForm.value);
   }
 
 }
