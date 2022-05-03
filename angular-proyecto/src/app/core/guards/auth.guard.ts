7d3962131229df1275083d4ced2e086a7e719681
JuanS1471 <86208230+JuanS1471@users.noreply.github.com>
2022-05-03 08:24:12 -0500

    AuthGuard

--- angular-proyecto/src/app/core/guards/auth.guard.ts
+++ angular-proyecto/src/app/core/guards/auth.guard.ts
@@ -1,15 +1,30 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { INTERNAL_ROUTES } from 'src/app/data/constants/routes';
+import { AuthService } from 'src/app/data/services/api/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  
+ constructor(
+   private router: Router,
+   private authService: AuthService
+ ){
+
+ }
+
+ canActivate(
+   next: ActivatedRouteSnapshot,
+   state: RouterStateSnapshot
+ ):boolean {
+     const currentUser = this.authService.getUser;
+     if(currentUser){
+       return true;
+     }
+     this.router.navigate([INTERNAL_ROUTES.AUTH_LOGIN],{
+       queryParams: {returnUrl: state.url}
+     });
+     return false
+ }
 }
