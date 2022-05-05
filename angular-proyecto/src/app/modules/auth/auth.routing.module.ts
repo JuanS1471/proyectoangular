f6387aa0fd69d60baf9baea73d337b76622502cd
JuanS1471 <86208230+JuanS1471@users.noreply.github.com>
2022-05-05 08:28:38 -0500

    Restringiendo rutas con guards

--- angular-proyecto/src/app/modules/auth/auth.routing.module.ts
+++ angular-proyecto/src/app/modules/auth/auth.routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { NoAuthGuard } from "src/app/core/guards/no-auth.guard";
+import { INTERNAL_PATHS } from "src/app/data/constants/routes";
 import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
     {
-        path: 'login',
-        component: LoginComponent
+        path: INTERNAL_PATHS.AUTH_LOGIN ,
+        component: LoginComponent,
+        canActivate: [NoAuthGuard]
     }
 ];
 
