76e28937331a60478e49bd0792ac05d81f9c4d58
JuanS1471 <86208230+JuanS1471@users.noreply.github.com>
2022-05-18 08:11:21 -0500

    Referencia desde template

--- angular-proyecto/src/app/modules/user/user-list/user-list.component.ts
+++ angular-proyecto/src/app/modules/user/user-list/user-list.component.ts
@@ -1,26 +1,13 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { ICardUser } from 'src/app/shared/components/cards/icard-user.metadata';
-import { USERS_DATA } from 'src/app/data/constants/users.const';
-import { CAROUSEL_DATA_ITEMS } from 'src/app/data/constants/carousel.const';
-import { ICarouselItem } from 'src/app/shared/components/carousel/Icarousel-item.metadata';
-import { UserService } from 'src/app/data/services/user.service';
-import { subscriptionLogsToBeFn } from 'rxjs/internal/testing/TestScheduler';
-import { Subscription } from 'rxjs';
+import { Component} from '@angular/core';
 import { SOLID_BUTTON_TYPE_ENUM } from 'src/app/shared/components/buttons/solid-button.type.enum';
-import { SolidButtonComponent } from 'src/app/shared/components';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent{
-  @ViewChild('mainbutton', null) mainbutton: SolidButtonComponent;
-  @ViewChild('container', null) container: ElementRef;
   public $btntypes = SOLID_BUTTON_TYPE_ENUM
   actions(events: SOLID_BUTTON_TYPE_ENUM){
-    // Send http request
-    // console.log(events)
-    this.mainbutton.title = 'actualizar';
-    this.container.nativeElement.style.display = 'none';
+   
   }
 }
