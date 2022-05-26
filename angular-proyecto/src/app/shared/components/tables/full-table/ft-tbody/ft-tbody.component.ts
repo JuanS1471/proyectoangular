2793b0925027beb3b94530e9148a8e2da95947be
JuanS1471 <86208230+JuanS1471@users.noreply.github.com>
2022-05-26 08:31:24 -0500

    Componente basado en servicios 3

--- angular-proyecto/src/app/shared/components/tables/full-table/ft-tbody/ft-tbody.component.ts
+++ angular-proyecto/src/app/shared/components/tables/full-table/ft-tbody/ft-tbody.component.ts
@@ -1,17 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FullTableService } from '../Services/full-table.service';
 
 @Component({
   selector: 'app-ft-tbody, [app-ft-tbody]',
   templateUrl: './ft-tbody.component.html',
   styleUrls: ['./ft-tbody.component.css']
 })
-export class FtTbodyComponent implements OnInit {
-
-  // @Input() data: Array<Array<string>> = []
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class FtTbodyComponent {
+  
 }
