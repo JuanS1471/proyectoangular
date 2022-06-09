139c84ff4f7b037a9987f3a170d2f7693f8b914f
JuanS1471 <86208230+JuanS1471@users.noreply.github.com>
2022-06-09 08:34:59 -0500

    Cambio de layout

--- angular-proyecto/src/app/layout/header/header.component.ts
+++ angular-proyecto/src/app/layout/header/header.component.ts
@@ -1,21 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { faBars, faBell, faComment } from '@fortawesome/free-solid-svg-icons';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/data/services/api/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
+export class HeaderComponent {
+  @Output() showMenu = new EventEmitter<any>();
   public faBars = faBars;
   public faBell = faBell;
-  public faComment =  faComment;
+  public faComment = faComment;
   public avatar = 'assets/images/defaults/avatar.jpg';
-  public logo =  'assets/images/defaults/logo.png'; 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  public logo = 'assets/images/defaults/logo.png';
+  public userSubscription!: Subscription;
+  constructor(
+    public authService: AuthService
+  ) { }
 }
--- angular-proyecto/src/app/layout/skeleton/skeleton.component.ts
+++ angular-proyecto/src/app/layout/skeleton/skeleton.component.ts
@@ -1,22 +1,27 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-skeleton',
   templateUrl: './skeleton.component.html',
   styleUrls: ['./skeleton.component.scss']
 })
-export class SkeletonComponent implements OnInit AfterViewInit {
-
+export class SkeletonComponent implements AfterViewInit, OnInit {
+  public showLeftNav = true;
+  public $theme = 'blue-dark';
   public loader = 'assets/images/loader/loader.gif';
   public isLoading = true;
-
   constructor() { }
 
   ngOnInit() {
   }
 
-  ngAfterViewInit(): void {
+  ngAfterViewInit() {
+    setTimeout(() => {
       this.isLoading = false;
+    }, 1000);
   }
 
+  showMenu() {
+    this.showLeftNav = !this.showLeftNav;
+  }
 }
